1f708c26823a30998db291df95b2d919e670eff2
Kez <yuijin0909@gmail.com>
2025-08-04 01:44:40 +0900

    해상도 대응

--- composeApp/src/wasmJsMain/kotlin/io/github/kez_lab/stopwatch_game/main.kt
+++ composeApp/src/wasmJsMain/kotlin/io/github/kez_lab/stopwatch_game/main.kt
@@ -1,21 +1,11 @@
 package io.github.kez_lab.stopwatch_game
 
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.text.font.toFontFamily
-import androidx.compose.ui.window.ComposeViewport
-import kotlinx.browser.document
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.preloadFont
-import stopwatch_game.composeapp.generated.resources.NotoSans
-import stopwatch_game.composeapp.generated.resources.Res
+import androidx.compose.ui.window.CanvasBasedWindow
 
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalResourceApi::class)
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    val body = document.body ?: return
-
-    ComposeViewport(body) {
-        val font by preloadFont(Res.font.NotoSans)
-        font?.toFontFamily()?.let { App(fontFamily = it) }
+    CanvasBasedWindow("ComposeTarget") {
+        App()
     }
 }
