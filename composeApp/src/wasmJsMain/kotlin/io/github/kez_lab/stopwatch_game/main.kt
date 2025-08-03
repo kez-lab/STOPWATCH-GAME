99f988de4ec2b8dc9368909acc687156f4b5b4bb
Kez <yuijin0909@gmail.com>
2025-08-04 01:45:51 +0900

    preload font 원복

--- composeApp/src/wasmJsMain/kotlin/io/github/kez_lab/stopwatch_game/main.kt
+++ composeApp/src/wasmJsMain/kotlin/io/github/kez_lab/stopwatch_game/main.kt
@@ -1,11 +1,23 @@
+@file:OptIn(
+    ExperimentalResourceApi::class,
+    ExperimentalResourceApi::class,
+    ExperimentalComposeUiApi::class
+)
+
 package io.github.kez_lab.stopwatch_game
 
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.text.font.toFontFamily
 import androidx.compose.ui.window.CanvasBasedWindow
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.preloadFont
+import stopwatch_game.composeapp.generated.resources.NotoSans
+import stopwatch_game.composeapp.generated.resources.Res
 
-@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     CanvasBasedWindow("ComposeTarget") {
-        App()
+        val font by preloadFont(Res.font.NotoSans)
+        font?.toFontFamily()?.let { App(fontFamily = it) }
     }
 }
